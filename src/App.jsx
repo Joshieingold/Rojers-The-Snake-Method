ab14ff7e697f88c6dedeae01892a9f0137ed4dab
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2024-10-22 19:01:01 -0300

    Micheal got fired so he is removed :( and cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+// Imports
 import React, { useState } from "react";
 import "./App.css";
 import joshSprite from "./assets/joshSprite.png";
 import maulikSprite from "./assets/maulkSprite.png";
-import michealSprite from "./assets/michealSprite.png";
 import tkSprite from "./assets/tkSprite.png";
 import Snake from "./Snake/snake.jsx";
 
+// Gamestate Variables
 const GameState = {
   TITLE: "TITLE",
   CHARACTERSELECT: "CHARACTERSELECT",
   PLAYING: "PLAYING",
   GAME_OVER: "GAME_OVER"
 };
-
+// Main function of the app.
 function App() {
   const [gameState, setGameState] = useState(GameState.TITLE);
   const [score, setScore] = useState(0); // Add a score state variable
@@ -22,54 +23,51 @@ function App() {
     setScore(0); // Reset score when starting a new game
     setGameState(GameState.PLAYING);
   };
-
+  // Handles character select link
   const selectCharacter = () => {
     setGameState(GameState.CHARACTERSELECT);
   };
-
   // Handle Game Over
   const gameOver = (finalScore) => {
     setScore(finalScore); // Update score on game over
     setGameState(GameState.GAME_OVER);
   };
-
   // Handle Restart Game
   const restartGame = () => {
     setGameState(GameState.PLAYING);
   };
-
+  // All the HTML based of GameState
   return (
-    <div className="App">
+    <div className="App"> 
+      {/* Div for title screen. */}
       {gameState === GameState.TITLE && (
         <div className="screenContainer">
-          <div className="title-container">
-            <h1 className="logo-text">Rojers</h1>
-          </div>
+          <h1 className="logo-text">Rojers</h1>
           <h2 className="sub-title">The Snake Method</h2>
           <button onClick={selectCharacter}>Start Game</button>
         </div>
       )}
+      {/* Div for Character select. */}
       {gameState === GameState.CHARACTERSELECT && (
         <div className="screenContainer">
-        <h1 className="title-text">Choose your Bom-Wipper!</h1>
-        <div className="charSelContainer">
-          <div className="joshContainer">
-            <button onClick={startGame} className="charBtn"><img src={joshSprite} className="sprite"></img></button>  
-            <h4 className="charName">Josh</h4>          
+          <h1 className="title-text">Choose your Bom-Wipper!</h1>
+          <div className="charSelContainer">
+            {/*Container for Josh Character */}
+            <div className="joshContainer">
+              <button onClick={startGame} className="charBtn"><img src={joshSprite} className="sprite"></img></button>  
+              <h4 className="charName">Josh</h4>          
+            </div>
+            {/*Container for TK Character */}
+            <div className="tkContainer">
+              <button onClick={startGame} className="charBtn"><img src={tkSprite} className="sprite"></img></button>  
+              <h4 className="charName">TK</h4>          
+            </div>
+            {/*Container for Maulik Character */}
+            <div className="MaulikContainer">
+              <button onClick={startGame} className="charBtn"><img src={maulikSprite} className="sprite"></img></button>  
+              <h4 className="charName">Maulik</h4>          
+            </div>
           </div>
-          <div className="tkContainer">
-            <button onClick={startGame} className="charBtn"><img src={tkSprite} className="sprite"></img></button>  
-            <h4 className="charName">TK</h4>          
-          </div>
-          <div className="michealContainer">
-            <button onClick={startGame} className="charBtn"><img src={michealSprite} className="sprite"></img></button>  
-            <h4 className="charName">Micheal</h4>          
-          </div>
-          <div className="MaulikContainer">
-            <button onClick={startGame} className="charBtn"><img src={maulikSprite} className="sprite"></img></button>  
-            <h4 className="charName">Maulik</h4>          
-          </div>
-        </div>
         </div>
 
       )}
